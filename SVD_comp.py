be4f37f208e071d4bd4fb18bc31a5e7b9ac5dfad
Ragul <uragul500@gmail.com>
2025-02-03 23:33:06 +0530

    # SVD comparision

--- SVD_comp.py
+++ SVD_comp.py
@@ -2,6 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
+# SVD comparision
+
 # Path to your PDB file
 pdb_file_path = r"C:\Amrita_S2\DSA proj\2c0k.pdb"  
 
