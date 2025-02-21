531fb2e57c63c3c524df6b323557137827662c4b
Ragul <uragul500@gmail.com>
2025-02-21 21:03:13 +0530

    updated

--- Scatter.py
+++ Scatter.py
@@ -11,7 +11,7 @@ def scatter_plot_preprocessed_data(data_file):
     fig = plt.figure(figsize=(10, 7))
     ax = fig.add_subplot(111, projection='3d')
     
-    ax.scatter(points[:, 0], points[:, 1], points[:, 2], c='b', marker='o', alpha=0.7)
+    ax.scatter(0, points[:, 1],0, c='b', marker='o', alpha=0.7)
     
     # Set labels for axes
     ax.set_title("3D Scatter Plot of Preprocessed Data")
@@ -20,7 +20,6 @@ def scatter_plot_preprocessed_data(data_file):
     ax.set_zlabel("Principal Component 3")
     plt.show()
 
-# Main function to load and plot the data
 data_file=r"C:\Amrita_S2\DSA proj\1A3N_ca_coordinates_preprocessed.csv"
-print(f"Loading preprocessed data from {data_file}...")    
+print(f"C:\Amrita_S2\DSA proj\1A3N_svd_features.csv")    
 scatter_plot_preprocessed_data(data_file)
